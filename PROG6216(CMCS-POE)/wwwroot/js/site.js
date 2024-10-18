2a03e762b60ba2ceafcafb1177ec1d8f2eb1b7f0
TakuMurwira <st10392257@vcconnect.edu.za>
2024-10-18 17:52:35 +0200

    Added a logo as well as colour and Fonts for user friendlyness

--- PROG6216(CMCS-POE)/wwwroot/js/site.js
+++ PROG6216(CMCS-POE)/wwwroot/js/site.js
@@ -49,15 +49,16 @@ setTimeout(function () {
     $('.alert').alert('close');
 }, 5000);
 
-function populateModal(claimID, lecturerID, submissionDate, hoursWorked, hourlyRate, totalClaimAmount, addNotes, documentNames) {
-    // Populate modal fields
-    document.getElementById('modalClaimID').textContent = claimID;
-    document.getElementById('modalLecturerID').textContent = lecturerID;
-    document.getElementById('modalSubmissionDate').textContent = submissionDate;
-    document.getElementById('modalHoursWorked').textContent = hoursWorked;
-    document.getElementById('modalHourlyRate').textContent = hourlyRate;
-    document.getElementById('modalTotalClaimAmount').textContent = totalClaimAmount;
-    document.getElementById('modalAddNotes').textContent = addNotes;
+function populateModal(claimID, lecturerID, submissionDate, hoursWorked, hourlyRate, totalClaimAmount, addNotes, documentNames, claimStatus) {
+    document.getElementById("modalClaimID").textContent = claimID;
+    document.getElementById("modalLecturerID").textContent = lecturerID;
+    document.getElementById("modalSubmissionDate").textContent = submissionDate;
+    document.getElementById("modalHoursWorked").textContent = hoursWorked;
+    document.getElementById("modalHourlyRate").textContent = hourlyRate;
+    document.getElementById("modalTotalClaimAmount").textContent = totalClaimAmount;
+    document.getElementById("modalAddNotes").textContent = addNotes;
+
+
 
     // Clear the document names list
     var documentList = document.getElementById('modalDocumentNames');
@@ -74,7 +75,7 @@ function populateModal(claimID, lecturerID, submissionDate, hoursWorked, hourlyR
         li.textContent = 'No documents uploaded';
         documentList.appendChild(li);
     }
-
+    document.getElementById("modalClaimStatus").textContent = claimStatus;
     // Set approve/reject button URLs
     document.getElementById('approveButton').setAttribute('href', '/Claim/Approve/' + claimID);
     document.getElementById('rejectButton').setAttribute('href', '/Claim/Reject/' + claimID);
